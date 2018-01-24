56aaf46c1c0ba9bb853ca134a496b61da560bd77
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2018-01-24 09:55:24 -0900

    [Section 8] - Completed Section 8. Setup Front/Back end Billing

--- Server/models/users.js
+++ Server/models/users.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleID: String
+  googleID: String,
+  credits: {
+    type: Number,
+    default: 0
+  }
 });
 
 mongoose.model("user", userSchema);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import Payments from "./Payments";
+
 class Header extends Component {
   renderContent = () => {
     switch (this.props.auth) {
@@ -14,23 +16,34 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="payments">
+            <Payments />
+          </li>,
+          <li style={{ margin: "0 10px" }} key="credits">
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="logout">
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   };
 
   render() {
-    return <nav>
+    return (
+      <nav>
         <div className="nav-wrapper">
-          <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
             Emaily
           </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
-      </nav>;
+      </nav>
+    );
   }
 }
 
